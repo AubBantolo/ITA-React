91d70e4addbdb2842be67d6df8725d3170edafec
AubBantolo <jhuliet14@gmail.com>
2023-08-07 15:04:27 +0800

    -A

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [submittedName, setSubmittedName] = useState('');
+
+  const cars = [
+        {id: 1, brand: 'Ford'},
+        {id: 2, brand: 'BMW'},
+        {id: 3, brand: 'Audi'}
+  ];
+
+  const inputName = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedName(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Enter your name: {submittedName} </p>
+     <form onSubmit={handleNameSubmit}>
+        <input id="name" type="text" value={name} onChange={inputName}/>
+        <input className="submit" type="submit" value="Login"/>
+      </form>
+
+    <h2>Who lives in my garage?</h2>
+            <ul>
+            {cars.map((car) => (
+              <li key={car.id}>
+                I am a {car.brand}
+              </li>
+            ))}
+          </ul>
     </div>
   );
 }
